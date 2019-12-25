db1a16ba516257dbf87455d420bbb3b960ddd8cc
qishaoxuan <474021214@qq.com>
2019-12-25 21:33:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { Slate, Editable, withReact } from 'slate-react'
 import { createEditor } from 'slate'
 
 const App = () => {
-  const editor = React.useMemo(() => withReact(createEditor()), [])
-  // Add the initial value when setting up our state.
-  const [value, setValue] = React.useState([
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState([
     {
       type: 'paragraph',
       children: [{ text: 'A line of text in a paragraph.' }],
     },
   ])
+  console.log('----------------------------')
+  console.log('editor', editor)
+  console.log('----------------------------')
 
   return (
     <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-      <Editable />
+      <Editable
+        onKeyDown={event => {
+          if (event.key === '&') {
+            // Prevent the ampersand character from being inserted.
+            event.preventDefault()
+            // Execute a command to insert text when the event occurs.
+            editor.apply({ type: 'insert_text', text: 'and' })
+          }
+        }}
+      />
     </Slate>
   )
 }
