4d5169e4cdabe9da59961a8e383dac6eca51e400
QiShaoXuan <474021214@qq.com>
2020-01-24 10:12:21 +0800

    add paragraph icon

--- src/blocks/paragraph/index.js
+++ src/blocks/paragraph/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useSelected } from 'slate-react';
 import __ from '@finxos/i18n';
-
+import Icon from './paragraph.svg';
 export default {
   name: 'paragraph',
   title: __('Paragraph'),
+  Icon: Icon,
   render: props => {
     return <p {...props.attributes}>{props.children}</p>;
   },
-  transform: props => {
+  save: props => {
     return 'default';
   },
 };
