9d9888f471a26cbdc9d75a2f0b0df872ffdb81d7
qishaoxuan <474021214@qq.com>
2020-02-12 17:31:25 +0800

    add global shortcut shift+enter

--- src/editor/shortcuts.js
+++ src/editor/shortcuts.js
@@ -1,1 +1,6 @@
-export default (event, editor) => {};
+export default (event, editor) => {
+  if (event.shiftKey && event.keyCode === 13) {
+    event.preventDefault();
+    editor.insertText('\n');
+  }
+};
