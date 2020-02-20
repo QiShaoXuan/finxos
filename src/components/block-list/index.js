b5ed1abe6648ffcf44b3f25ded513f6fbead3706
qishaoxuan <474021214@qq.com>
2020-02-20 16:42:18 +0800

    update

--- src/components/block-list/index.js
+++ src/components/block-list/index.js
@@ -25,7 +25,7 @@ export default props => {
           <li
             key={to.name}
             onMouseDown={e => {
-              e.preventDefault();
+              // e.preventDefault();
               to.before && to.before(editor);
               transformBlock(
                 editor,
